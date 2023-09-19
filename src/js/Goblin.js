c3e0c3cf70b48c463eee8eda0c1c0f6ea773b034
Alex n <Shakal240@gmail.com>
2023-09-19 21:56:17 +0300

    task ready

---
+++ src/js/Goblin.js
@@ -0,0 +1,21 @@
+export default class Goblin {
+  constructor(gameElement) {
+    this.cells = gameElement.querySelectorAll('.cage');
+    this.goblinHead = gameElement.querySelector('.head');
+
+  }
+
+  rotate() {
+    let previous = 0;
+    let rand = 0;
+
+    setInterval(() => {
+      while (rand === previous) {
+        rand = Math.floor(Math.random() * (this.cells.length -1));
+      }
+      previous = rand;
+      this.cells[rand].appendChild(this.goblinHead);
+
+    }, 1000);
+  }
+}
